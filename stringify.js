5c857c23a7bf833fb1c649d9715d8a188615859d
vkz <vladilen.kozin@gmail.com>
2014-05-11 18:59:24 +0400

    Test on array of Int: get metrics with d8, node, Chrome profiler.

--- stringify.js
+++ stringify.js
@@ -28,3 +28,49 @@ function mystringify(obj, indent) {
     }
     return buf.join('');
 }
+
+// Returns a random integer between min and max
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var MIN_ARRAY_LENGTH = 100,
+    MAX_ARRAY_LENGTH = 1000,
+    NOF_INPUTS        = 100,
+    NOF_RUNS          = 10;
+
+// [1, 2, 3, ..] int array, but .forEach() maybe too general
+function makeFakeArray() {
+    var a = [];
+    var l = randomBetween(MIN_ARRAY_LENGTH, MAX_ARRAY_LENGTH);
+    var start = randomBetween(MIN_ARRAY_LENGTH, MAX_ARRAY_LENGTH);
+
+    for (var i = 0; i < l; i++) {
+        a.push(start + i);
+    }
+    return a;
+}
+
+var fakeInput = [];
+var total_fakeNumbers = 0;
+for (var i = 0; i < NOF_INPUTS; i++) {
+    fakeInput.push(makeFakeArray());
+    total_fakeNumbers += (fakeInput[i]).length;
+}
+
+var start = Date.now();
+for (var j = 0; j < NOF_RUNS; j++) {
+    mystringify(fakeInput);
+}
+var end = Date.now();
+
+var timeSpent = end - start;
+var totalNumbers = total_fakeNumbers * NOF_RUNS;
+var result = [timeSpent + ' ms', Math.floor(totalNumbers/timeSpent) + ' numbers/ms'];
+
+if (typeof console !== 'undefined' && typeof document !== 'undefined') {
+    console.log(result[0], ', ', result[1]);
+    document.writeln(result[0], ', ', result[1]);
+} else if (typeof console !== 'undefined' ) {
+    console.log(result[0], ', ', result[1]);
+}
